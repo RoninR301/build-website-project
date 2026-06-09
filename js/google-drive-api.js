0f81dc724511ec6473d00742889e51ce457b74b1
RoninR301 <136247765+RoninR301@users.noreply.github.com>
2026-06-09 22:30:11 +0530

    Update google-drive-api.js

--- js/google-drive-api.js
+++ js/google-drive-api.js
@@ -1,71 +1,172 @@
 const GOOGLE_DRIVE_API =
-
 "https://www.googleapis.com/drive/v3";
 
 async function getDriveFolder(
-folderId
+    folderId
 ) {
 
-const token =
-getCurrentDriveToken();
+    const token =
+    getCurrentDriveToken();
 
-if (!token) {
+    if (!token) {
 
-    throw new Error(
-        "Drive Not Connected"
-    );
-}
-
-const response =
-await fetch(
-
-`${GOOGLE_DRIVE_API}/files/${folderId}`,
-
-{
-    headers: {
-
-        Authorization:
-        `Bearer ${token}`
-
+        throw new Error(
+            "Drive Not Connected"
+        );
     }
-}
 
-);
+    const response =
+    await fetch(
 
-return await response.json();
+        `${GOOGLE_DRIVE_API}/files/${folderId}`,
+
+        {
+            headers: {
+
+                Authorization:
+                `Bearer ${token}`
+
+            }
+        }
+
+    );
+
+    return await response.json();
 
 }
 
 async function listDriveFiles(
-folderId
+    folderId
 ) {
 
-const token =
-getCurrentDriveToken();
+    const token =
+    getCurrentDriveToken();
 
-if (!token) {
+    if (!token) {
 
-    throw new Error(
-        "Drive Not Connected"
-    );
-}
-
-const response =
-await fetch(
-
-`${GOOGLE_DRIVE_API}/files?q='${folderId}'+in+parents`,
-
-{
-    headers: {
-
-        Authorization:
-        `Bearer ${token}`
-
+        throw new Error(
+            "Drive Not Connected"
+        );
     }
+
+    const response =
+    await fetch(
+
+        `${GOOGLE_DRIVE_API}/files?q='${folderId}'+in+parents`,
+
+        {
+            headers: {
+
+                Authorization:
+                `Bearer ${token}`
+
+            }
+        }
+
+    );
+
+    return await response.json();
+
 }
 
+async function createDriveFolder(
+    folderName
+) {
+
+    const token =
+    getCurrentDriveToken();
+
+    if (!token) {
+
+        throw new Error(
+            "Drive Not Connected"
+        );
+    }
+
+    const response =
+    await fetch(
+
+        "https://www.googleapis.com/drive/v3/files",
+
+        {
+            method: "POST",
+
+            headers: {
+
+                Authorization:
+                `Bearer ${token}`,
+
+                "Content-Type":
+                "application/json"
+
+            },
+
+            body: JSON.stringify({
+
+                name:
+                folderName,
+
+                mimeType:
+                "application/vnd.google-apps.folder"
+
+            })
+
+        }
+
+    );
+
+    return await response.json();
+
+}
+
+async function getDriveFile(
+    fileId
+) {
+
+    const token =
+    getCurrentDriveToken();
+
+    if (!token) {
+
+        throw new Error(
+            "Drive Not Connected"
+        );
+    }
+
+    const response =
+    await fetch(
+
+        `${GOOGLE_DRIVE_API}/files/${fileId}`,
+
+        {
+            headers: {
+
+                Authorization:
+                `Bearer ${token}`
+
+            }
+        }
+
+    );
+
+    return await response.json();
+
+}
+
+async function createProjectDriveFolder(
+projectName
+) {
+
+const folder =
+
+await createDriveFolder(
+    projectName
 );
 
-return await response.json();
+setDriveFolderId(
+    folder.id
+);
+
+return folder;
 
 }
